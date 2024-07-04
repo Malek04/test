05e7290f1f5869a7e5d4d91b960b313e0529f3c0
user <malekaiter04@gmail.com>
2024-07-04 17:29:16 +0100

    first commit

---
+++ PokemonReviewApp/Controllers/ReviewerController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewerController(IReviewerRepository reviewerRepository, IMapper mapper)
+        {
+            _reviewerRepository = reviewerRepository;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Reviewer>))]
+        public IActionResult GetReviewers()
+        {
+            var reviewers = _mapper.Map<List<ReviewerDto>>(_reviewerRepository.GetReviewers());
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviewers);
+        }
+        [HttpGet("{ReviewerId}")]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        [ProducesResponseType(400)]
+        public IActionResult GerReviewer(int ReviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(ReviewerId))
+                return NotFound();
+            var reviewer = _mapper.Map<PokemonDto>(_reviewerRepository.GetReviewer(ReviewerId));
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(reviewer);
+        }
+        [HttpGet("{ReviewerId}/reviews")]
+        public IActionResult GetReviewsByreviewer(int ReviewerId)
+        {
+            if (!_reviewerRepository.ReviewerExists(ReviewerId))
+                return NotFound();
+            var reviews = _mapper.Map<List<ReviewDto>>(_reviewerRepository.GetReviewsByreviewer(ReviewerId));
+            if (!ModelState.IsValid)
+                return BadRequest();
+            return Ok(reviews);
+        }
+
+    }
+}
---
+++ PokemonReviewApp/Interfaces/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int Reviewid);
+        ICollection<Review> GetReviewsOfAPokemon(int PokemonId);
+        bool ReviewExists(int Reviewid);
+    }
+}
